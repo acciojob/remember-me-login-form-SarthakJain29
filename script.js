f0c923be7d40d2b59a52e799067b9de0c399dce3
Sarthak Jain <93116216+SarthakJain29@users.noreply.github.com>
2025-06-13 14:03:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//your JS code here. If required.
+const user = document.getElementById("username");
+const pass = document.getElementById("password");
+const remember = document.getElementById("remember");
+const submit = document.getElementById("submit");
+const exist = document.getElementById("existing");
+
+if(localStorage.getItem("username")){
+	exist.style.visibility = "visible";
+}
+exist.addEventListener("click", (e) => {
+	e.preventDefault();
+	const user1 = localStorage.getItem("username");
+	alert(`Logged in as ${user1}`);
+})
+submit.addEventListener("click", (e) => {
+	e.preventDefault();
+	alert(`Logged in as ${user.value}`);
+	if(remember.checked){
+		localStorage.setItem("Username", user.value);
+		localStorage.setItem("Password", pass.value);
+	}else{
+		localStorage.clear();
+	}
+})
